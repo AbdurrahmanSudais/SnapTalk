567d4978e35a6518ea92559302b6d0f90bdb1802
AbdurrahmanSudais <abdurrahmansudais539@gmail.com>
2025-05-05 18:24:21 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyApKEx-bYKOqB80mlWr53up9iyIiCzv2aI",
+  authDomain: "snaptalk-b8369.firebaseapp.com",
+  projectId: "snaptalk-b8369",
+  storageBucket: "snaptalk-b8369.appspot.com",
+  messagingSenderId: "442098306088",
+  appId: "1:442098306088:web:280c8615656b8e4d3af91d"
 };
 
 export const app = initializeApp(firebaseConfig);
