601048c12cbf8d5bfe90546e3968258aa9f9fb1b
AbdurrahmanSudais <abdurrahmansudais539@gmail.com>
2025-07-21 14:33:57 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,10 @@ import {
   getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
   signOut, onAuthStateChanged, updateProfile
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 import {
   getFirestore, doc, setDoc, getDoc, collection, addDoc,
   onSnapshot, serverTimestamp, query, orderBy
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 import {
   getStorage, ref, uploadBytes, getDownloadURL
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
@@ -19,7 +17,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyApKEx-bYKOqB80mlWr53up9iyIiCzv2aI",
   authDomain: "snaptalk-b8369.firebaseapp.com",
   projectId: "snaptalk-b8369",
-  storageBucket: "snaptalk-b8369.appspot.com", // corrected the domain
+  storageBucket: "snaptalk-b8369.appspot.com",
   messagingSenderId: "442098306088",
   appId: "1:442098306088:web:280c8615656b8e4d3af91d"
 };
@@ -39,4 +37,3 @@ export {
   onSnapshot, serverTimestamp, query, orderBy,
   ref, uploadBytes, getDownloadURL
 };
- 
